4726d517a104d83a927ed66a922e18003c94992b
Charith <charithm3010@gmail.com>
2026-09-24 19:22:16 +0530

    syntax error is corrected

--- monitor.py
+++ monitor.py
@@ -1,4 +1,4 @@
-"print("checking pump status")" 
-"print("fuel level normal")" 
+print("checking pump status") 
+print("fuel level normal") 
 print('fuel price corrected') 
 print('LOW FUEL ALERT triggered') 
